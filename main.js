ba2dcd0f2fe695835e33fdee40a3d0371c6935b6
Michael Currin <michael.currin@gmail.com>
2020-07-14 12:43:41 +0200

    style: Update main.js

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ const API_BASE_URL = 'https://api.reelgood.com/v2',
 const TV = 'show',
     MOVIE = 'movie';
 
-
 /** Return URL for poster image or placeholder image. **/
 function makeImageUrl(hasPoster, type, id, large) {
     var pixels = large ? 342 : 92;
 
+    var url;
     if (hasPoster) {
-        var url = `${IMG_BASE_URL}/${type}/${id}/poster-${pixels}.webp`;
-    } else {
-        var url = `${IMG_PLACEHOLDER_BASE_URL}/${pixels}x${pixels * 1.5}?text=No+image`;
+        url = `${IMG_BASE_URL}/${type}/${id}/poster-${pixels}.webp`;
+    }
+    else {
+        url = `${IMG_PLACEHOLDER_BASE_URL}/${pixels}x${pixels * 1.5}?text=No+image`;
     }
 
     return url;
 }
-
 
 /** Extract useful fields from a show and also add image URLs. **/
 function parseShow(show) {
@@ -39,26 +39,22 @@ function parseShow(show) {
         overview: show.overview,
         released_on: show.released_on,
         genres: show.genres,
-        season_count: show.season_count,
+        season_count: show.season_count
     };
 }
 
-
 function getData() {
-    return $.getJSON(API_QUERY_URL)
-        .then(result => result.map(parseShow))
+    return $.getJSON(API_QUERY_URL).then(result => result.map(parseShow));
 }
-
 
 /** Fetch data and add it to the HTML using templating. **/
 function render() {
-    getData()
-        .then(data => {
-            var content = {
-                "shows": data
-            }
-            var template = $("#shows").html();
-            var html = Mustache.to_html(template, content);
-            $('#target-output').html(html);
-        });
+    getData().then(data => {
+        var content = {
+            shows: data
+        };
+        var template = $('#shows').html();
+        var html = Mustache.to_html(template, content);
+        $('#target-output').html(html);
+    });
 }
