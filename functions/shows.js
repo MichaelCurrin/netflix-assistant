d20b75637806e8bcfda76d10df9fdda01521c962
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-26 21:55:45 +0200

    Create shows.js

---
+++ functions/shows.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+const API_BASE_URL = 'https://api.reelgood.com/v2',
+    API_QUERY_URL = `${API_BASE_URL}/browse/source/netflix?sort=4&sources=netflix&take=250`;
+
+
+exports.handler = async (event, context) => {
+   const res = await axios.get(API_QUERY_URL)
+
+   return {
+      statusCode: res.status,
+      body: res.data
+   }
+}
