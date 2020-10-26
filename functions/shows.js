f2dc246e6db9df44024a6f8d28f0bcef57f0984c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-26 22:28:22 +0200

    Update shows.js

--- functions/shows.js
+++ functions/shows.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 
-exports.handler = async (event, context) => {
-   const body = JSON.parse(event.body)
+exports.handler = async (event, _) => {
+  const body = JSON.parse(event.body)
 
-   const res = await axios.get(body.url)
+  const res = await axios.get(body.url)
 
-   return {
-      statusCode: res.status,
-      body: JSON.stringify(res.data)
-   }
+  return {
+    statusCode: res.status,
+    body: JSON.stringify(res.data)
+  }
 }
