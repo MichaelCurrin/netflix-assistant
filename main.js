87bdc92110cacdc6f3e588d7da80aa0d2a13f322
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-26 22:27:17 +0200

    style: Update formatting

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
 const API_BASE_URL = 'https://api.reelgood.com/v2',
-    API_QUERY_URL = `${API_BASE_URL}/browse/source/netflix?sort=4&sources=netflix&take=250`,
-    LAMBDA_URL = '/.netlify/functions/shows',
-    IMG_BASE_URL = 'https://img.reelgood.com/content',
-    IMG_PLACEHOLDER_BASE_URL = 'https://via.placeholder.com';
+  API_QUERY_URL = `${API_BASE_URL}/browse/source/netflix?sort=4&sources=netflix&take=250`,
+  LAMBDA_URL = '/.netlify/functions/shows',
+  IMG_BASE_URL = 'https://img.reelgood.com/content',
+  IMG_PLACEHOLDER_BASE_URL = 'https://via.placeholder.com';
 
 const TV = 'show',
-    MOVIE = 'movie';
+  MOVIE = 'movie';
 
 /** Return URL for poster image or placeholder image. **/
 function makeImageUrl(hasPoster, type, id, large) {
-    var pixels = large ? 342 : 92;
+  var pixels = large ? 342 : 92;
 
-    var url;
-    if (hasPoster) {
-        url = `${IMG_BASE_URL}/${type}/${id}/poster-${pixels}.webp`;
-    }
-    else {
-        url = `${IMG_PLACEHOLDER_BASE_URL}/${pixels}x${pixels * 1.5}?text=No+image`;
-    }
+  var url;
+  if (hasPoster) {
+    url = `${IMG_BASE_URL}/${type}/${id}/poster-${pixels}.webp`;
+  }
+  else {
+    url = `${IMG_PLACEHOLDER_BASE_URL}/${pixels}x${pixels * 1.5}?text=No+image`;
+  }
 
-    return url;
+  return url;
 }
 
 /** Extract useful fields from a show and also add image URLs. **/
 function parseShow(show) {
-    var isMovie = show.content_type == 'm';
-    var type = isMovie ? MOVIE : TV;
-    var id = show.id;
-    var imgLarge = makeImageUrl(show.has_poster, type, id, true);
-    var imgSmall = makeImageUrl(show.has_poster, type, id, false);
+  var isMovie = show.content_type == 'm';
+  var type = isMovie ? MOVIE : TV;
+  var id = show.id;
+  var imgLarge = makeImageUrl(show.has_poster, type, id, true);
+  var imgSmall = makeImageUrl(show.has_poster, type, id, false);
 
-    return {
-        id: id,
-        type: type,
-        image_large: imgLarge,
-        image_small: imgSmall,
-        slug: show.slug,
-        title: show.title,
-        overview: show.overview,
-        released_on: show.released_on,
-        genres: show.genres,
-        season_count: show.season_count
-    };
+  return {
+    id: id,
+    type: type,
+    image_large: imgLarge,
+    image_small: imgSmall,
+    slug: show.slug,
+    title: show.title,
+    overview: show.overview,
+    released_on: show.released_on,
+    genres: show.genres,
+    season_count: show.season_count
+  };
 }
 
 function getData(url) {
-    const params = { url: url };
-    
-    return $.getJSON(LAMBDA_URL, params)
-        .then(result => result.map(parseShow));
+  const params = { url: url };
+
+  return $.getJSON(LAMBDA_URL, params)
+    .then(result => result.map(parseShow));
 }
 
 /** Fetch data and add it to the HTML using templating. **/
 function render() {
-    getData(API_QUERY_URL).then(data => {
-        var template = $('#shows').html();
-        
-        var content = {
-            shows: data
-        };
-        var html = Mustache.to_html(template, content);
-        $('#target-output').html(html);
-    });
+  getData(API_QUERY_URL).then(data => {
+    var template = $('#shows').html();
+
+    var content = {
+      shows: data
+    };
+    var html = Mustache.to_html(template, content);
+    $('#target-output').html(html);
+  });
 }
