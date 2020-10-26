f63dbd0aab9a627ace7dd339fb6028f450d731aa
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-26 22:43:11 +0200

    Update shows.js

--- functions/shows.js
+++ functions/shows.js
@@ -1,12 +1,20 @@
 const axios = require('axios')
 
+const API_BASE_URL = 'https://api.reelgood.com/v2';
+
 exports.handler = async (event) => {
   const url = event.queryStringParameters.name
 
   if (!url) {
     return {
-      statusCode: '400',
+      statusCode: 400,
       body: 'Missing param: url'
+    }
+  }
+  if (!API_BASE_URL.startsWith(API_BASE_URL)) {
+    return {
+      statusCode: 400,
+      body: `Domain not allowed - only: ${API_BASE_URL}`
     }
   }
 
