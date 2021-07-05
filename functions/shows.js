3fd00c9fb8673b3259cabc6a0ebad30bf9aecebb
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-05 11:14:23 +0200

    Update shows.js

--- functions/shows.js
+++ functions/shows.js
@@ -1,16 +1,23 @@
+/**
+ * Shows Function.
+ *
+ * Note use of cache of computed data, to improve performance and
+ * reduce the number of hitss down against the external API we are consuming.
+ */
 const axios = require("axios");
 
 const API_BASE_URL = "https://api.reelgood.com/v2";
 
-exports.handler = async (event) => {
+exports.handler = async function(event) {
   const url = event.queryStringParameters.url;
 
   if (!url) {
     return {
       statusCode: 400,
-      body: "Missing param: url",
+      body: "Missing param: 'url'",
     };
   }
+  
   if (!API_BASE_URL.startsWith(API_BASE_URL)) {
     return {
       statusCode: 400,
@@ -19,8 +26,7 @@ exports.handler = async (event) => {
   }
 
   const res = await axios.get(url);
-
-  // Use cache to reduce hits on the external API.
+  
   return {
     statusCode: res.status,
     body: JSON.stringify(res.data),
