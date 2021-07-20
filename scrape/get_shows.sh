cb401d46b3c8a3e60cd5cc6418a0b72ab2e9c948
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-20 11:25:23 +0200

    style: update get_shows.sh

--- scrape/get_shows.sh
+++ scrape/get_shows.sh
@@ -12,7 +12,11 @@ PARAMS='availability=onSources&content_kind=show&free=false&hide_seen=false&hide
 
 curl "$URL?$PARAMS" \
   -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' \
-  -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://reelgood.com/tv?filter-imdb_start=5.2&filter-rg_start=53' -H 'x-platform: web' -H 'Origin: https://reelgood.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' |
+  -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' \
+  --compressed \
+  -H 'Referer: https://reelgood.com/tv?filter-imdb_start=5.2&filter-rg_start=53' \
+  -H 'x-platform: web' -H 'Origin: https://reelgood.com' -H 'DNT: 1' -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' |
   python3 -m json.tool \
     >out/shows.json
 
@@ -20,7 +24,12 @@ curl "$URL?$PARAMS" \
 PARAMS='availability=onSources&content_kind=show&free=false&hide_seen=false&hide_tracked=false&hide_watchlisted=false&imdb_end=10&imdb_start=5.0&region=us&rg_end=100&rg_start=50&skip=50&sort=0&sources=netflix&take=250&year_end=2021&year_start=1900'
 
 curl "$URL?$PARAMS" \
-  -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://reelgood.com/tv?filter-imdb_start=5.2&filter-rg_start=51' -H 'x-platform: web' -H 'Origin: https://reelgood.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' |
+  -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' \
+  -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' \
+  --compressed \
+  -H 'Referer: https://reelgood.com/tv?filter-imdb_start=5.2&filter-rg_start=51' \
+  -H 'x-platform: web' -H 'Origin: https://reelgood.com' -H 'DNT: 1' -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' |
   python3 -m json.tool \
     >out/shows2.json
 
